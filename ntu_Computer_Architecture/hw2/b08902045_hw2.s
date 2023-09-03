2515196084576b671a3cff227abd3c40c72d1023
tkyen <tkyen1110@gmail.com>
2023-09-04 04:41:19 +0800

    Add ntu_Computer_Architecture/hw2

---
+++ ntu_Computer_Architecture/hw2/b08902045_hw2.s
@@ -0,0 +1,108 @@
+.globl __start
+
+.rodata
+    division_by_zero: .string "division by zero"
+    remainder_by_zero: .string "remainder by zero"
+
+.text
+__start:
+    # Read first operand
+    li a0, 5
+    ecall
+    mv s0, a0
+    # Read operation
+    li a0, 5
+    ecall
+    mv s1, a0
+    # Read second operand
+    li a0, 5
+    ecall
+    mv s2, a0
+
+###################################
+#  TODO: Develop your calculator  #
+# If 𝑜𝑝 = 0, calculate 𝐴 + 𝐵 and output the result.
+# If 𝑜𝑝 = 1, calculate 𝐴 − 𝐵 and output the result.
+# If 𝑜𝑝 = 2, calculate 𝐴 × 𝐵 and output the result.
+# If 𝑜𝑝 = 3, calculate 𝐴 / 𝐵 and output the result. (Quotient).
+# If 𝑜𝑝 = 4, calculate 𝐴 % 𝐵 and output the result. (Remainder)
+# If 𝑜𝑝 = 5, calculate 𝐴 ^ B and output the result.
+# If 𝑜𝑝 = 6, calculate 𝐴! and output the result. (In this case, 𝐵 = 0)
+slti x5, s1, 1
+bne x5, x0, PLUS
+slti x5, s1, 2
+bne x5, x0, MINUS
+slti x5, s1, 3
+bne x5, x0, MULT
+slti x5, s1, 4
+bne x5, x0, DIVI
+slti x5, s1, 5
+bne x5, x0, MOD
+slti x5, s1, 6
+bne x5, x0, POW
+slti x5, s1, 7
+bne x5, x0, FACT
+
+PLUS:
+    add s3, s0, s2
+    beq x0, x0, output
+MINUS:
+    sub s3, s0, s2
+    beq x0, x0, output
+MULT:
+    mul s3, s0, s2
+    beq x0, x0, output
+DIVI:
+    beq x0, s2, division_by_zero_except
+    div s3, s0, s2
+    beq x0, x0, output
+MOD:
+    beq x0, s2, remainder_by_zero_except
+    rem s3, s0, s2
+    beq x0, x0, output
+POW:
+    addi s3, x0, 1 # s3 = 1
+    slti x6, s2, 1
+    bne x0, x6, output
+    beq x0, x0, POWLOOP
+POWLOOP:
+    addi s4, x0, 1 # s4 = 1
+    sub s2, s2, s4 # s2 = s2 - s4 = s2 - 1
+    mul s3, s3, s0
+    beq s2, x0, output
+    beq x0, x0, POWLOOP
+FACT:
+    addi s3, x0, 1 # s3 = 1
+    slti x6, s0, 2
+    bne x0, x6, output
+    beq x0, x0, FACTLOOP
+FACTLOOP:
+    mul s3, s3, s0
+    addi s4, x0, 1 # s4 = 1
+    sub s0, s0, s4 # s0 = s0 - s4 = s0 - 1
+    beq s0, x0, output
+    beq x0, x0 FACTLOOP
+###################################
+
+output:
+    # Output the result
+    li a0, 1
+    mv a1, s3
+    ecall
+
+exit:
+    # Exit program(necessary)
+    li a0, 10
+    ecall
+
+division_by_zero_except:
+    li a0, 4
+    la a1, division_by_zero
+    ecall
+    jal zero, exit
+
+remainder_by_zero_except:
+    li a0, 4
+    la a1, remainder_by_zero
+    ecall
+    jal zero, exit
---
+++ ntu_Computer_Architecture/hw2/yinhsuan.s
@@ -0,0 +1,112 @@
+.globl __start
+
+.rodata
+    division_by_zero: .string "division by zero"
+    remainder_by_zero: .string "remainder by zero"
+    JumpTable: .word plus, minus, mult, divd, mod, pow, frac
+
+.text
+__start:
+    # Read first operand
+    li a0, 5    # li rd, immediate, 5 is represented the system call service code to read
+    ecall       # set the kernal mode back to 0
+    mv s0, a0   # addi rd, rs, 0
+    # Read operation
+    li a0, 5
+    ecall
+    mv s1, a0
+    # Read second operand
+    li a0, 5
+    ecall
+    mv s2, a0
+
+###################################
+#  TODO: Develop your calculator  #
+#                                 #
+###################################
+
+#s0:first, s1:operator, s2:second
+
+# check operator and goto
+la s4, JumpTable    # s4 == jumptable[0]
+slli s5, s1, 2      # s5 == s1 << 2, s1 == operator
+add s6, s5, s4      # s6 == jumptable + s1 * 4
+lw s7, 0(s6)
+jr s7
+
+# let s11 be the answer
+plus:
+    add s11, s0, s2
+    jal x1, output
+
+minus:
+    sub s11, s0, s2
+    jal x1, output
+
+mult:
+    mul s11, s0, s2
+    jal x1, output
+
+divd:
+    beq s2, x0, division_by_zero_except
+    div s11, s0, s2
+    jal x1, output
+
+mod:
+    beq s2, x0, remainder_by_zero_except
+    div s11, s0, s2     # s11 = s0 / s2
+    mul s4, s11, s2     # s4 = s2 * s11
+    sub s11, s0, s4    # s11 = s0 - s4
+    jal x1, output
+
+pow:
+    mv s11, s0
+    bne s2, x0, pow_loop    # if x0 != 0, goto pow_loop
+    addi s11, x0, 1         # else s11 = 1 and goto output
+    jal x0, output          
+pow_loop:
+    addi s3, x0, 1      # s2 = s2-1
+    sub s2, s2, s3
+    mul s11, s11, s0
+    beq s2, x0, output
+    jal x0, pow_loop
+
+frac:
+    mv s11, s0
+    bne s0, x0, frac_loop   # if s0 != 0, then goto frac_loop
+    addi s11, x0, 1         # else s11 = 1, then goto output
+    jal x0, output
+    
+frac_loop:
+    addi s3, x0, 1      # s3 = 1
+    sub s0, s0, s3      # s0 = s0-1
+
+    beq s0, x0, output
+    mul s11, s11, s0    
+    jal x0, frac_loop
+
+
+output:
+    # Output the result
+    li a0, 1        # a0: Function arguments
+    mv a1, s11      # a1: return values  
+    ecall
+
+exit:
+    # Exit program(necessary)
+    li a0, 10
+    ecall
+
+division_by_zero_except:
+    li a0, 4
+    la a1, division_by_zero
+    ecall
+    jal zero, exit
+
+remainder_by_zero_except:
+    li a0, 4
+    la a1, remainder_by_zero
+    ecall
+    jal zero, exit
+
+
